3b8019fa616c7ba3ee5cfa8b769cbd2f4827d580
Doğukan Uygun <111589592+dgkanygn@users.noreply.github.com>
2022-09-25 20:36:09 +0300

    Update database.js

--- server/database.js
+++ server/database.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const connectDB = async () => {
   try {
-    // MONGOOSE=mongodb+srv://dgkanuygun:alemdar_polat26@cluster0.keypga6.mongodb.net/commerce?retryWrites=true&w=majority
+    
 
     await mongoose.connect(process.env.MONGOOSE);
     console.log("db connection successful");
